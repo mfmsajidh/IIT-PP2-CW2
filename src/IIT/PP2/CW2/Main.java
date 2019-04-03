05baef6e9ec0f7d07a29144dfdd15763917848a1
Sajidh Farook <mohamedsajidh.2018851@iit.ac.lk>
2019-04-03 14:59:22 +0530

    Added Application Icon

--- src/IIT/PP2/CW2/Main.java
+++ src/IIT/PP2/CW2/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,6 +14,11 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXML/EmployeeDetails.fxml"));
         primaryStage.setTitle("Computer Consultancy Firm");
         primaryStage.setScene(new Scene(root, 600, 400));
+
+        //Sets an icon for the application
+        Image applicationIcon = new Image(getClass().getResourceAsStream("Images/Logo.png"));
+        primaryStage.getIcons().add(applicationIcon);
+
         primaryStage.show();
     }
 
