84967946e01a8a062edc574b73bcf26efb2b6fcb
Sajidh Farook <mohamedsajidh.2018851@iit.ac.lk>
2019-04-03 15:27:13 +0530

    Added Function for login button

--- src/IIT/PP2/CW2/Controller/WelcomeController.java
+++ src/IIT/PP2/CW2/Controller/WelcomeController.java
@@ -1,4 +1,33 @@
 package IIT.PP2.CW2.Controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
 public class WelcomeController {
+
+    @FXML
+    private Button btn_login;
+
+    Stage primaryStage = new Stage();
+
+    public void goToEmployeeDetails() throws Exception{
+//      get the current window
+        Stage stage = (Stage)btn_login.getScene().getWindow();
+
+//      close the current window
+        stage.close();
+
+//      load the attendance list window
+        Parent root = FXMLLoader.load(getClass().getResource("../FXML/EmployeeDetails.fxml"));
+        primaryStage.setTitle("Employee Details");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+
+    }
+
+
 }
