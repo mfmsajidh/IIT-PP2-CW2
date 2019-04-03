e6ed4d12343fd321f210fece085a93a9cfbd9e2d
Sajidh Farook <mohamedsajidh.2018851@iit.ac.lk>
2019-04-03 15:13:16 +0530

    Added Welcome Screen - Login

--- src/IIT/PP2/CW2/Main.java
+++ src/IIT/PP2/CW2/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("FXML/EmployeeDetails.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FXML/Welcome.fxml"));
         primaryStage.setTitle("Computer Consultancy Firm");
         primaryStage.setScene(new Scene(root, 600, 400));
 
@@ -21,7 +21,6 @@ public class Main extends Application {
 
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
