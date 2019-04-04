fb27c2a6e6455413e131ec4094267f94c6f0f206
Sajidh Farook <mohamedsajidh.2018851@iit.ac.lk>
2019-04-04 14:45:13 +0530

    Finalized navigation bar for each UI

--- src/main/java/IIT/PP2/CW2/Controller/CustomerDetailsController.java
+++ src/main/java/IIT/PP2/CW2/Controller/CustomerDetailsController.java
@@ -1,4 +1,104 @@
 package IIT.PP2.CW2.Controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
 public class CustomerDetailsController {
+
+    @FXML
+    private Button btn_employees;
+    @FXML
+    private Button btn_contracts;
+    @FXML
+    private Button btn_jobRoles;
+    @FXML
+    private Button btn_jobTypes;
+
+    private Stage primaryStage = new Stage();
+
+    public void goToEmployeeDetails() throws Exception{
+//        Gets the current window
+        Stage currentStage = (Stage)btn_employees.getScene().getWindow();
+
+//        Closes the current window
+        currentStage.close();
+
+//        Loads the job type details window
+        Parent root = FXMLLoader.load(getClass().getResource("/IIT/PP2/CW2/FXML/EmployeeDetails.fxml"));
+        primaryStage.setTitle("BeGOOD Inc. - Employee Details");
+        primaryStage.setScene(new Scene(root, 600, 400));
+
+//        Sets an icon for the application
+        Image applicationIcon = new Image(getClass().getResourceAsStream("/IIT/PP2/CW2/Images/Logo.png"));
+        primaryStage.getIcons().add(applicationIcon);
+
+        primaryStage.show();
+
+    }
+
+    public void goToContractDetails() throws Exception{
+//        Gets the current window
+        Stage currentStage = (Stage)btn_contracts.getScene().getWindow();
+
+//        Closes the current window
+        currentStage.close();
+
+//        Loads the contract details window
+        Parent root = FXMLLoader.load(getClass().getResource("/IIT/PP2/CW2/FXML/ContractDetails.fxml"));
+        primaryStage.setTitle("BeGOOD Inc. - Contract Details");
+        primaryStage.setScene(new Scene(root, 600, 400));
+
+//        Sets an icon for the application
+        Image applicationIcon = new Image(getClass().getResourceAsStream("/IIT/PP2/CW2/Images/Logo.png"));
+        primaryStage.getIcons().add(applicationIcon);
+
+        primaryStage.show();
+
+    }
+
+    public void goToJobRoleDetails() throws Exception{
+//        Gets the current window
+        Stage currentStage = (Stage)btn_jobRoles.getScene().getWindow();
+
+//        Closes the current window
+        currentStage.close();
+
+//        Loads the job role details window
+        Parent root = FXMLLoader.load(getClass().getResource("/IIT/PP2/CW2/FXML/JobRoleDetails.fxml"));
+        primaryStage.setTitle("BeGOOD Inc. - Job Role Details");
+        primaryStage.setScene(new Scene(root, 600, 400));
+
+//        Sets an icon for the application
+        Image applicationIcon = new Image(getClass().getResourceAsStream("/IIT/PP2/CW2/Images/Logo.png"));
+        primaryStage.getIcons().add(applicationIcon);
+
+        primaryStage.show();
+
+    }
+
+    public void goToJobTypeDetails() throws Exception{
+//        Gets the current window
+        Stage currentStage = (Stage)btn_jobTypes.getScene().getWindow();
+
+//        Closes the current window
+        currentStage.close();
+
+//        Loads the job type details window
+        Parent root = FXMLLoader.load(getClass().getResource("/IIT/PP2/CW2/FXML/JobTypeDetails.fxml"));
+        primaryStage.setTitle("BeGOOD Inc. - Job Type Details");
+        primaryStage.setScene(new Scene(root, 600, 400));
+
+//        Sets an icon for the application
+        Image applicationIcon = new Image(getClass().getResourceAsStream("/IIT/PP2/CW2/Images/Logo.png"));
+        primaryStage.getIcons().add(applicationIcon);
+
+        primaryStage.show();
+
+    }
+
 }
