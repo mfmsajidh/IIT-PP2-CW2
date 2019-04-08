6ad18d0cea06a852ff05abef64948e39a52fe184
Sajidh Farook <mohamedsajidh.2018851@iit.ac.lk>
2019-04-08 16:08:15 +0530

    Fixed Date

--- src/main/java/IIT/PP2/CW2/DTO/EmployeeDetailsDTO.java
+++ src/main/java/IIT/PP2/CW2/DTO/EmployeeDetailsDTO.java
@@ -4,7 +4,6 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 public class EmployeeDetailsDTO {
 
