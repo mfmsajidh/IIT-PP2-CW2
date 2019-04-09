b888a87fc2bfdd4c27971d16a989648aca0b7031
Sajidh Farook <mohamedsajidh.2018851@iit.ac.lk>
2019-04-09 08:34:48 +0530

    Added Login Script

--- src/main/java/IIT/PP2/CW2/Controller/WelcomeScreenController.java
+++ src/main/java/IIT/PP2/CW2/Controller/WelcomeScreenController.java
@@ -4,12 +4,24 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 
 public class WelcomeScreenController {
 
+    private final static String username = "admin";
+    private final static String password = "admin";
+
+    @FXML
+    private Label lbl_status;
+    @FXML
+    private TextField txt_username;
+    @FXML
+    private PasswordField txt_password;
     @FXML
     private Button btn_login;
     @FXML
@@ -18,22 +30,30 @@ public class WelcomeScreenController {
     private Stage primaryStage = new Stage();
 
     public void goToEmployeeDetails() throws Exception{
-//        Gets the current window
-        Stage currentStage = (Stage)btn_login.getScene().getWindow();
 
-//        Closes the current window
-        currentStage.close();
+        String enteredUsername = txt_username.getText();
+        String enteredPassword = txt_password.getText();
 
-//        Loads the jobRole details window
-        Parent root = FXMLLoader.load(getClass().getResource("/IIT/PP2/CW2/FXML/EmployeeDetailsUI.fxml"));
-        primaryStage.setTitle("BeGOOD Inc. - Employee Details");
-        primaryStage.setScene(new Scene(root, 900, 400));
+        if (enteredUsername.equals(username) && enteredPassword.equals(password)) {
+//            Gets the current window
+            Stage currentStage = (Stage)btn_login.getScene().getWindow();
 
-//        Sets an icon for the application
-        Image applicationIcon = new Image(getClass().getResourceAsStream("/IIT/PP2/CW2/Images/Logo.png"));
-        primaryStage.getIcons().add(applicationIcon);
+//            Closes the current window
+            currentStage.close();
 
-        primaryStage.show();
+//            Loads the jobRole details window
+            Parent root = FXMLLoader.load(getClass().getResource("/IIT/PP2/CW2/FXML/EmployeeDetailsUI.fxml"));
+            primaryStage.setTitle("BeGOOD Inc. - Employee Details");
+            primaryStage.setScene(new Scene(root, 900, 400));
+
+//            Sets an icon for the application
+            Image applicationIcon = new Image(getClass().getResourceAsStream("/IIT/PP2/CW2/Images/Logo.png"));
+            primaryStage.getIcons().add(applicationIcon);
+
+            primaryStage.show();
+        } else {
+            lbl_status.setText("Incorrect Username / Password !!!");
+        }
 
     }
 
@@ -41,6 +61,5 @@ public class WelcomeScreenController {
         Stage currentStage = (Stage)btn_exit.getScene().getWindow();
         currentStage.close();
     }
-    
 
 }
